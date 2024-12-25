3a412a960a05db8825cfb6fd71cc7307b902585e
Vinayak Joshi <vinayak.joshi@rummyverse.com>
2024-12-25 08:10:16 +0530

    updated the code to use stream API for satisfy class

--- src/main/java/utils/vinayak/patterns/SatisfyAny.java
+++ src/main/java/utils/vinayak/patterns/SatisfyAny.java
@@ -10,11 +10,10 @@ public class SatisfyAny<K,V> implements Condition<K,V> {
 
     @Override
     public boolean satisfies(Getter<K,V> data) {
-        for (Condition<K,V> condition : conditions) {
-            if (condition.satisfies(data)) {
-                return true;
-            }
-        }
-        return false;
+        return !conditions
+                .stream()
+                .filter(condition -> condition.satisfies(data))
+                .findFirst()
+                .isEmpty();
     }
 }
